bc3c31fd1a1a006275579f3f8313dc0640207a92
dqv28 <dovuong020802@gmail.com>
2024-01-02 17:26:16 +0700

    Update

--- src/pages/ArticleForm/MenuItemTable.tsx
+++ src/pages/ArticleForm/MenuItemTable.tsx
@@ -3,7 +3,7 @@ import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
 import { ModalForm, ProFormItem, ProFormItemProps, ProFormText } from '@ant-design/pro-components';
 import { Button, Table } from 'antd';
 import { uniqueId } from 'lodash';
-import React from 'react';
+import React, { useState } from 'react';
 
 type FormData = {
   name: string;
@@ -15,6 +15,7 @@ const MenuItemTable: React.FC<{
   onChange?: (value: any) => void;
 }> = ({ value, onChange }) => {
   const id = uniqueId();
+  const [initData, setInitData] = useState([]);
   const handleAddMenu = async (formData: FormData) => {
     onChange?.([
       ...(value ?? []),
@@ -31,6 +32,8 @@ const MenuItemTable: React.FC<{
   return (
     <div>
       <Table
+        rowKey="id"
+        rowSelection={{}}
         dataSource={value}
         columns={[
           { title: 'name', dataIndex: 'name' },
@@ -44,66 +47,85 @@ const MenuItemTable: React.FC<{
                     if (!menu) {
                       return;
                     }
-                    // menu.map((item: Menu) => {
-                    //   if (item.id === record.id) {
-                    const old = menu.map((item: any) =>
+
+                    const newMenu: Menu[] = menu.map((item: any) =>
                       item.id === record.id
                         ? {
                             ...item,
                             children: [
-                              ...item.children,
+                              ...(item.children ?? []),
                               {
                                 id,
-                                name: `child ${uniqueId()}`,
+                                name: `child ${id}`,
                                 path: 'child',
                                 children: [],
                               },
                             ],
                           }
-                        : addChildren(item.children),
+                        : {
+                            ...item,
+                            children: addChildren(item.children),
+                          },
                     );
-                    console.log(old);
+
+                    return newMenu;
                   };
 
-                  //   if (item.children) {
-                  //     addChildren(item.children);
-                  //   }
-                  // });
-
-                  //   return menu;
-                  // };
-
                   onChange?.(addChildren(value));
-                  // const map = (item) => {
-                  //   if (item.id === record.id) {
-                  //     return {
-                  //       ...item,
-                  //       children: [
-                  //         ...item.children,
-                  //         {
-                  //           id: uniqueId(),
-                  //           children: [],
-                  //         },
-                  //       ],
-                  //     };
-                  //   } else {
-                  //     return {
-                  //       ...item,
-                  //       children: item.children.map(map),
-                  //     };
-                  //   }
-                  // };
-
-                  // onChange?.(value.map(map));
                 }}
                 icon={<PlusOutlined />}
               />,
-              <Button type="link" icon={<EditOutlined />} />,
+              <Button
+                type="link"
+                icon={<EditOutlined />}
+                onClick={() => {
+                  const editChild = (menu: Menu[]) => {
+                    if (!menu) {
+                      return;
+                    }
+
+                    const newMenu: Menu[] = menu.map((item: Menu) => {
+                      if (item.id === record.id) {
+                        return {
+                          ...item,
+                          name: 'Child updated',
+                        };
+                      } else {
+                        return {
+                          ...item,
+                          children: editChild(item.children ?? []),
+                        };
+                      }
+                    });
+
+                    return newMenu;
+                  };
+
+                  onChange?.(editChild(value));
+                }}
+              />,
               <Button
                 type="link"
                 onClick={() => {
-                  console.log(record);
-                  onChange?.(value.filter((item: Menu) => item.id !== record.id));
+                  const removeChild = (menu: Menu[]) => {
+                    if (!menu) {
+                      return;
+                    }
+
+                    let newMenu;
+
+                    newMenu = menu.filter((item: Menu) => {
+                      if (item.id === record.id) {
+                        return item.id !== record.id;
+                      } else {
+                        item.children = removeChild(item.children ?? []);
+                        return true;
+                      }
+                    });
+
+                    return newMenu;
+                  };
+                  onChange?.(removeChild(value));
                 }}
                 icon={<DeleteOutlined />}
               />,
--- src/pages/ArticleForm/index.tsx
+++ src/pages/ArticleForm/index.tsx
@@ -1,7 +1,6 @@
 import { ProForm, ProFormText } from '@ant-design/pro-components';
 import MenuItemFormItem from './MenuItemTable';
 import React from 'react';
-import { uniqueId } from 'lodash';
 
 export type TableListItem = {
   key: number;
@@ -12,78 +11,6 @@ const ArticleForm: React.FC = () => {
   const handleSubmit = async (form: any) => {
     console.log('Form', form);
   };
-
-  var menu = [
-    {
-      id: 1,
-      name: 'menu',
-      path: 'menu',
-      children: [
-        {
-          id: 2,
-          name: 'child',
-          path: 'child',
-          children: [],
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: 'menu 2',
-      path: 'menu2',
-      children: [],
-    },
-    {
-      id: 4,
-      name: 'menu 3',
-      path: 'menu3',
-      children: [
-        {
-          id: 5,
-          name: 'child 2',
-          path: 'child2',
-          children: [
-            {
-              id: 6,
-              name: 'child 3',
-              path: 'child3',
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-  ];
-
-  const addChildren = (menu: any) => {
-    if (!menu) {
-      return;
-    }
-
-    menu.map((item: any) => {
-      if (item.name === 'child 3') {
-        item.children = [
-          ...item.children,
-          {
-            id: 7,
-            name: 'child 4',
-            path: 'child4',
-            children: [],
-          },
-        ];
-        console.log('1');
-      }
-
-      if (item.children) {
-        addChildren(item.children);
-      }
-    });
-
-    return menu;
-  };
-
-  const newMenu = addChildren(menu);
-  console.log(newMenu);
 
   return (
     <>
